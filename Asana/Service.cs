4431639dda9e92cb0688c0f67b6370972582dee7
Attila Köteles <attila.koteles@gmail.com>
2019-09-04 21:17:25 +0200

    Add project files.

---
+++ Asana/Service.cs
@@ -0,0 +1,121 @@
+﻿using Asana.Mapping;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asana
+{
+    public class Service
+    {
+        private HttpClient _client;
+        private string _personalAccessToken;
+
+        public Service(string personalAccessToken)
+        {
+            _personalAccessToken = personalAccessToken;
+            _client = CreateClient();
+        }
+
+        private HttpClient CreateClient()
+        {
+            var client = new HttpClient()
+            {
+                BaseAddress = new Uri("https://app.asana.com/api/1.0/")
+            };
+
+            // specify to use TLS 1.2 as default connection
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + _personalAccessToken);
+            client.DefaultRequestHeaders.Add("Asana-Disable", "string_ids");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+
+        public async Task<List<User>> GetUsersAsync()
+        {
+            var response = await _client.GetAsync("users");
+            response.EnsureSuccessStatusCode();
+
+            var jObject = JObject.Parse(await response.Content.ReadAsStringAsync());
+            var jToken = jObject.GetValue("data");
+            List<User> users = (List<User>)jToken.ToObject(typeof(List<User>));
+            return users;
+        }
+
+        public async Task<long> CreateTaskAsync(long workspaceId, long assignee, string name, string notes)
+        {
+            var req = new
+            {
+                data = new
+                {
+                    workspace = workspaceId,
+                    assignee = assignee,
+                    name = name,
+                    notes = notes
+                }
+            };
+
+            var res = _client.PostAsync("tasks", GetJsonContent(req)).Result;
+            res.EnsureSuccessStatusCode();
+
+            return GetIdFromResponse(await res.Content.ReadAsStringAsync());
+        }
+
+        public async Task AddTagToTaskAsync(long taskId, long tagId)
+        {
+            await PostObjectToUrl(
+                $"tasks/{taskId}/addTag",
+                new
+                {
+                    data = new
+                    {
+                        tag = tagId
+                    }
+                });
+        }
+
+        public async Task<long> AddCommentToTaskAsync(long taskId, string text)
+        {
+            return GetIdFromResponse(await PostObjectToUrl(
+                $"tasks/{taskId}/stories",
+                new
+                {
+                    data = new
+                    {
+                        text = text
+                    }
+                }));
+        }
+
+        private StringContent GetJsonContent(object obj)
+        {
+            return new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+        }
+
+        private async Task<string> PostObjectToUrl(string url, object obj)
+        {
+            var response = await _client.PostAsync(url, GetJsonContent(obj));
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        private long GetIdFromResponse(string responseJson)
+        {
+            var jObj = JObject.Parse(responseJson);
+
+            var id = jObj["data"]["id"].ToString();
+            if (id == null)
+                throw new NullReferenceException("Can't parse service result - no ID returned!");
+
+            return long.Parse(id);
+        }
+    }
+}
---
+++ AsanaTest/ServiceTest.cs
@@ -0,0 +1,59 @@
+﻿using Asana;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AsanaTest
+{
+    public class ServiceTest
+    {
+        // You need to fill these variables with your values!
+        const string PersonalAccessToken = "FILL-THIS-OUT";
+        const int WorkspaceId = 0;
+        const int UserToAssign = 0;
+        const int TagId = 0;
+
+        private static Service asanaService;
+
+        public ServiceTest()
+        {
+            asanaService = new Service(PersonalAccessToken);
+        }
+
+        [Fact]
+        public void TestGetUsersAsync()
+        {
+            var users = asanaService.GetUsersAsync().Result;
+            Assert.True(users.Count > 0);
+        }
+
+        [Fact]
+        public void TestCreateTaskAsync()
+        {
+            var taskId = HelperCreateTestTask();
+            Assert.True(taskId > 1_000_000);
+        }
+
+        [Fact]
+        public void TestAddTagToTaskAsync()
+        {
+            var taskId = HelperCreateTestTask();
+            asanaService.AddTagToTaskAsync(taskId, TagId).Wait();
+        }
+
+        [Fact]
+        public void TestAddCommentToTaskAsync()
+        {
+            var taskId = HelperCreateTestTask();
+            var commentId = asanaService.AddCommentToTaskAsync(taskId, "This is a test comment").Result;
+            Assert.True(commentId > 1_000_000);
+        }
+
+        private long HelperCreateTestTask()
+        {
+            return asanaService.CreateTaskAsync(WorkspaceId, UserToAssign, "Unit test task", "Some notes").Result;
+        }
+
+    }
+}
